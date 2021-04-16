cd96a633da875d0059de0663401aa7c1183c1aae
risification <sultangaziev01@bk.ru>
2021-04-16 19:29:36 +0600

    add router and export_file from ExportView

--- import_export_app/urls.py
+++ import_export_app/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register(r'document', DocFileModelViewSet)
 
 urlpatterns = [
     path('', DocumentView.as_view()),
+    path('', include(router.urls)),
+    path('export_file/<int:document_id>/', ExportView.as_view()),
 ]
