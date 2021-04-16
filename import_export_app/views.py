12f4c4e53d295184ab2097bacec0b3c0bfedac54
risification <sultangaziev01@bk.ru>
2021-04-16 19:27:36 +0600

    add in view file - ExportView and DocFileModelViewSet

--- import_export_app/views.py
+++ import_export_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
 from .admin import DocumentResources
 import tablib
-from import_export import resources
+from rest_framework import viewsets
 
 
 # Create your views here.
@@ -15,11 +15,35 @@ class DocumentView(APIView):
         document = Document.objects.all()
         dataset = DocumentResources().export()
         serializers = DocumentSerializers(document, many=True)
-        with open('file.txt', 'wt', encoding='utf-8') as infile:
-            infile.writelines(str(dataset))
+        with open('media/file.csv', 'wt', encoding='utf-8') as infile:
+            infile.writelines(str(dataset.csv))
         return Response(serializers.data)
 
-    '''def post(self, request, *args, **kwargs):
-        serializers = DocumentSerializers(user=request.user)
+    def post(self, request, *args, **kwargs):
+        serializers = DocumentSerializers(data=request.data)
         if serializers.is_valid():
-            dataset = DocumentResources().export()'''
+            serializers.save()
+            return Response({"data": "OK"}, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors)
+
+
+from django.core.files import File
+
+
+class ExportView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        document = Document.objects.get(id=kwargs['document_id'])
+        serializers = DocumentSerializers(document)
+        return Response(serializers.data)
+
+    def post(self, request, *args, **kwargs):
+        serializers = DownloadSerializers(data=request.data)
+        if serializers.is_valid():
+            return Response({"data": 1})
+        return Response(serializers.errors)
+
+
+class DocFileModelViewSet(viewsets.ModelViewSet):
+    serializer_class = DownloadSerializers
+    queryset = DocumentField.objects.all()
