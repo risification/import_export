c62c3f31ddf936f194d3ef6d206b6279b93f6cda
risification <sultangaziev01@bk.ru>
2021-04-16 19:31:56 +0600

    add DocumentAdmin

--- import_export_app/admin.py
+++ import_export_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
 from import_export.fields import Field
-from .models import Document
-from import_export import resources
+from .models import Document, DocumentField
+from import_export import resources, fields, widgets
+from django.dispatch import receiver
+from import_export.signals import post_import, post_export
 
 
 # Register your models here.
@@ -10,11 +13,24 @@ from import_export import resources
 class DocumentResources(resources.ModelResource):
     # date_expired = Field(attribute='date_expired', column_name='date_testing')
     # myfield = Field(column_name='myfield')
-    full_title = Field()
+    # full_title = Field()
+    delete = fields.Field(widget=widgets.BooleanWidget())
+
+    def for_delete(self, row, instance):
+        return self.fields['delete'].clean(row)
 
     class Meta:
         model = Document
-        fields = ('id__title__text__user')
+        fields = ('id__title__text__user__target__date_expired')
+
+        @receiver(post_import, dispatch_uid='balabala...')
+        def _post_import(model, **kwargs):
+            pass
+
+        @receiver(post_export, dispatch_uid='balabala...')
+        def _post_export(model, **kwargs):
+            pass
+
         '''widjets = {
             'date': {'format': '%d.%m.%Y'}
         }'''
@@ -28,4 +44,9 @@ class DocumentResources(resources.ModelResource):
         return '%s by %s' % (document.title, document.text)'''
 
 
-admin.site.register(Document)
+class DocumentAdmin(ImportExportActionModelAdmin):
+    list_display = ['title', 'text', 'date', 'user', 'target']
+
+
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(DocumentField)
