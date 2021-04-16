8b907d6c34d4a2e6232749e2ea69e6014db3c868
risification <sultangaziev01@bk.ru>
2021-04-16 19:30:44 +0600

    add DownloadSerializer

--- import_export_app/serializers.py
+++ import_export_app/serializers.py
@@ -1,10 +1,24 @@
+from csv import DictReader
+
 from rest_framework import serializers
-from .models import *
+from .models import Document, DocumentField
 
 
 class DocumentSerializers(serializers.ModelSerializer):
     class Meta:
         model = Document
-        fields = ['id', 'title', 'text', 'date', 'user', 'target', 'date_expired']
+        fields = '__all__'
 
 
+class DownloadSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = DocumentField
+        fields = ['file_field']
+
+    def create(self, validated_data):
+        file_data = validated_data.pop('file_field')
+        document = Document.objects.create(**validated_data)
+        for file in file_data:
+            document(document=document, **file)
+            document.save()
+        return file_data
