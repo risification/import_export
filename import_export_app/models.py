5e510a3bc7b680475fc8d9a8cb5a82fa06fd5f00
risification <sultangaziev01@bk.ru>
2021-04-16 19:31:03 +0600

    add DocumentField

--- import_export_app/models.py
+++ import_export_app/models.py
@@ -9,6 +9,23 @@ class Document(models.Model):
     title = models.CharField(max_length=30)
     text = models.TextField()
     date = models.DateField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     target = models.CharField(max_length=40)
-    date_expired = models.DateField()
+    # date_expired = models.DateField()
+
+
+'''class CsvDocument(CsvModel):
+    title = CharField()
+    text = CharField()
+    date = DateField()
+    user = IntegerField()
+    target = CharField()
+    date_expired = CharField()
+
+    class Meta:
+        delimiter = ","
+'''
+
+
+class DocumentField(models.Model):
+    file_field = models.FileField()
